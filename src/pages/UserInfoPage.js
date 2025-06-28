154908ca9d49c71e71df23c4dd80c6b1e308ddf4
EunBi Ko <e_un_04@naver.com>
2025-06-28 22:44:54 +0900

    디자인 수정

--- src/pages/UserInfoPage.js
+++ src/pages/UserInfoPage.js
@@ -1,11 +1,15 @@
+import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
+import MainpageModal from "../components/MainpageModal"; // ✅ 모달 컴포넌트 import
+
 import {
-  Container,
   BackIcon,
   ContentWrapper,
-  LogoBox,
-  LogoImage,
-  LogoText,
+  StoreHeader,
+  StorePageWrapper,
+  StoreLogoGroup,
+  StoreLogoImage,
+  StoreLogoText,
   InfoCard,
   ProfileImage,
   UserName,
@@ -19,51 +23,59 @@ import {
   WithdrawBox,
   WithdrawText,
   WithdrawLine,
+  StoreMenuIcon,
 } from "../styles/UserInfoPageStyle";
 
 const UserInfoPage = () => {
-    const navigate = useNavigate(); // 훅 호출
-
-    const handleGoHome = () => {
-        navigate('/'); // 홈으로 이동
-    };
+  const [isOpen, setIsOpen] = useState(false); 
+  const [isLoggedIn, setIsLoggedIn] = useState(true);
+  const navigate = useNavigate();
 
   return (
-    <Container>
-      <BackIcon
-        src="뒤로가는화살표.svg"
-        alt="뒤로가기"
-        onClick={handleGoHome}
-      />
+    <StorePageWrapper>
+      <StoreHeader>
+        <BackIcon
+          src={process.env.PUBLIC_URL + "/뒤로가는화살표.svg"}
+          alt="뒤로가기"
+          onClick={() => navigate("/")}
+        />
+        <StoreLogoGroup>
+          <StoreLogoImage
+            src={process.env.PUBLIC_URL + "/로고마늘.svg"}
+            alt="로고"
+          />
+          <StoreLogoText>의성:Check</StoreLogoText>
+        </StoreLogoGroup>
+        <StoreMenuIcon
+          src={process.env.PUBLIC_URL + "/menu.svg"}
+          alt="메뉴"
+          onClick={() => setIsOpen(true)}
+        />
+      </StoreHeader>
 
       <ContentWrapper>
-        <LogoBox>
-          <LogoImage src="로고마늘.svg" alt="로고" />
-          <LogoText>의성:Check</LogoText>
-        </LogoBox>
-
         <InfoCard>
-          <ProfileImage src="프로필.svg" alt="프로필" />
+          <ProfileImage src="/프로필.svg" alt="프로필" />
           <UserName>홍길동</UserName>
 
           <InfoSectionTitle>내 정보</InfoSectionTitle>
           <Divider />
 
           <InfoRow>
-            <InfoIcon src="아이디-아이콘.svg" alt="아이디" />
+            <InfoIcon src="/아이디-아이콘.svg" alt="아이디" />
             <InfoText>홍길동</InfoText>
           </InfoRow>
 
           <InfoRow>
-            <InfoIcon src="메일-아이콘.svg" alt="이메일" />
+            <InfoIcon src="/메일-아이콘.svg" alt="이메일" />
             <InfoText>kkas123@gmail.com</InfoText>
           </InfoRow>
 
           <InfoRow>
-            <InfoIcon src="내예약-아이콘.svg" alt="예약" />
+            <InfoIcon src="/내예약-아이콘.svg" alt="예약" />
             <InfoText>내 예약</InfoText>
             <img
-              src="오른화살표.svg"
+              src="/오른화살표.svg"
               alt="화살표"
               width="20"
               style={{ cursor: "pointer" }}
@@ -81,7 +93,20 @@ const UserInfoPage = () => {
           </WithdrawBox>
         </InfoCard>
       </ContentWrapper>
-    </Container>
+
+      {/* ✅ 메뉴 모달 열림 */}
+      {isOpen && (
+        <MainpageModal
+          isLoggedIn={isLoggedIn}
+          onClose={() => setIsOpen(false)}
+          onLoginClick={() => navigate("/login")}
+          onLogoutClick={() => setIsLoggedIn(false)}
+          onLangClick={() => alert("준비중입니다.")}
+          onMyPageClick={() => navigate("/mypage")}
+          onStoreRegisterClick={() => navigate("/addstore")}
+        />
+      )}
+    </StorePageWrapper>
   );
 };
 
